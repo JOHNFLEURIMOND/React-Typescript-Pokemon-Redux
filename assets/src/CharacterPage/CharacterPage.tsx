e65449db8ff45b3aa323eca125a00da74bcf4b34
johnfleurimond <johncfleurimond@gmail.com>
2022-01-12 10:51:42 -0500

    Backend is connected to frontend

--- assets/src/CharacterPage/CharacterPage.tsx
+++ assets/src/CharacterPage/CharacterPage.tsx
@@ -10,51 +10,52 @@ import Footer from "../Footer/Footer";
 import SearchBar from "../SearchBar/SearchBar";
 import { fleurimondColors } from "../theme";
 
-const baseBannerStyles = css({
-  boxSizing: "border-box",
-  minWidth: 0,
-  fontSize: "1rem",
-  color: fleurimondColors.black,
-  lineHeight: "normal",
-  fontWeight: 600,
-  margin: 0,
-  padding: "0",
-  height: "700px",
-  width: "100%",
-  backgroundColor: fleurimondColors.white,
 
-  ".leftHalf": {
-    width: "50%",
-    display: "inline-block",
-  },
-  ".rightHalf": {
-    width: "50%",
-    display: "inline-block",
-  },
-  "*": {
-    fontFamily: "Montserrat, sans-serif",
-  },
-});
+  const baseBannerStyles = css({
+    boxSizing: "border-box",
+    minWidth: 0,
+    fontSize: "1rem",
+    color: fleurimondColors.black,
+    lineHeight: "normal",
+    fontWeight: 600,
+    margin: 0,
+    padding: "0",
+    height: "700px",
+    width: "100%",
+    backgroundColor: fleurimondColors.white,
 
-const JFCharacterCards = (props): JSX.Element => {
-  return (
-    <Container {...props}>
-      <Nav />
-      <Animated
-        animationInDelay={0}
-        animationIn="slideInUp"
-        animationOut="slideOutDown"
-        isVisible
-      >
-        <SearchBar />
-      </Animated>
-      <Footer />
-    </Container>
-  );
-};
+    ".leftHalf": {
+      width: "50%",
+      display: "inline-block",
+    },
+    ".rightHalf": {
+      width: "50%",
+      display: "inline-block",
+    },
+    "*": {
+      fontFamily: "Montserrat, sans-serif",
+    },
+  });
 
-const JFCharacterCardsWithStyle = styled(JFCharacterCards, {
-  shouldForwardProp,
-})(baseBannerStyles, space, flexbox, typography);
+  const JFCharacterCards = (props: any): JSX.Element => {
+    return (
+      <Container {...props}>
+        <Nav />
+        <Animated
+          animationInDelay={0}
+          animationIn="slideInUp"
+          animationOut="slideOutDown"
+          isVisible
+        >
+          <SearchBar />
+        </Animated>
+        <Footer />
+      </Container>
+    );
+  };
 
-export default JFCharacterCardsWithStyle;
+  const JFCharacterCardsWithStyle = styled(JFCharacterCards, {
+    shouldForwardProp,
+  })(baseBannerStyles, space, flexbox, typography);
+
+  export default JFCharacterCardsWithStyle;
