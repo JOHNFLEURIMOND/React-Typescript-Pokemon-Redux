d68f4eaa37d457c667a81816b673a0925cb3dab1
johnfleurimond <johncfleurimond@gmail.com>
2022-01-29 17:46:09 -0500

    Need to style about me page

--- assets/src/Footer/Footer.tsx
+++ assets/src/Footer/Footer.tsx
@@ -1,91 +1,183 @@
-import React from "react";
-import { css } from "@emotion/core";
-import styled from "@emotion/styled";
-import shouldForwardProp from "@styled-system/should-forward-prop";
-import { space, flexbox, typography } from "styled-system";
-import { Container } from "semantic-ui-react";
+import React, { useState } from 'react';
 import moment from "moment";
-
+import { GlobalStyle, Container } from '../layout/global-style';
+import styled from 'styled-components';
 import { fleurimondColors } from "../theme";
+import { Link } from 'react-router-dom';
 
-const baseFooterStyles = css({
-  boxSizing: "border-box",
-  fontSize: "20px",
-  height: "6vw",
-  textAlign: "center",
-  width: "100vw",
-  backgroundColor: fleurimondColors.buttons.darkBlue,
-  minHeight: "8vh",
-  position: "fixed",
-  display: "flex",
-  padding: "20px",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  left: "0",
-  bottom: "0",
-  overflowX: "hidden",
-  overflowY: "hidden",
-  overflow: "hidden",
-  zIndex: 100,
+export const Footer = styled.footer` 
+width: 100%;
+height: 10vh;
+box-sizing: border-box;
+display: flex;
+align-items: center;
+text-align: center;
+justify-content: center;
+font-size: 20px;
+background: linear-gradient(
+  90deg,
+  ${fleurimondColors.black} 0%,
+  ${fleurimondColors.smoke} 27%,
+  ${fleurimondColors.graySmoke} 85%
+);
+padding: 20px;
+`;
 
-  "App-footer": {
-    fontSize: "20px",
-    height: "55px",
-    color: fleurimondColors.leapingLemon,
-    textAlign: "center",
-    padding: "20px",
-    position: "absolute",
-  },
+export const FooterContainer = styled(Container)`
+display: flex;
+justify-content: space-between;
+align-items: center;
+height: 80px;
+`;
 
-  a: {
-    textDecoration: "none",
-    color: fleurimondColors.leapingLemon,
-    "&:hover,&:active,&:focus": {
-      borderColor: fleurimondColors.white,
-      color: fleurimondColors.white,
-    },
-  },
-  ul: {
-    width: "52vw",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    margin: "2%",
-    listStyle: "none",
-  },
-  "*": {
-    fontFamily: "Montserrat, sans-serif",
-  },
-});
+export const FooterLogo = styled(Link)`
+color: ${fleurimondColors.white};
+cursor: pointer;
+display: flex;
+align-items: center;
+text-decoration: none;
+font-size: 2.5rem;
+font-weight: 800;
+transition: all .5s ease;
+&:hover,&:active,&:focus{
+    border-color: ${fleurimondColors.white};
+    color: ${fleurimondColors.white};
+    transform: scale(1.08);
+}
+`;
 
-const JFProjects = (props): JSX.Element => {
+
+
+export const MenuIcon = styled.div`
+display: none;
+@media (max-width: 1000px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-50%, 20%);
+    font-size: 4rem;
+    cursor: pointer;
+}
+`;
+
+export const Menu = styled.ul`
+display: flex;
+align-items: center;
+text-align: center;
+width: 100vw;
+flex-direction: row;
+justify-content: space-around;
+margin: 2%;
+listStyle: none;
+@media only screen and (max-width:1000px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100vh;
+    position: absolute;
+    top: 80px;
+    left: ;
+    background-color: rgba(0, 0, 0, 0.9);
+    transition: all .5s ease;
+}
+`;
+
+export const MenuItem = styled.li`
+list-style: none;
+height: 80px;
+@media only screen and (max-width:1000px){
+    width: 100%;
+    &:hover {
+        border: none;
+    }
+}
+`;
+
+export const MenuLink = styled(Link)`
+text-decoration: none;
+font-weight: bold;
+font-size: 2rem;
+color: #fff;
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 1rem 2rem;
+height: 100%;
+transition: all .2s ease;
+&:hover {
+    color: ${fleurimondColors.palesasAqua};
+    transform: traslateY(-3rem);
+}
+&:active {
+    transform: traslateY(3rem);
+    color: ${fleurimondColors.palesasAqua};
+}
+@media only screen and (max-width:1000px){
+    display: block;
+    padding: 3rem;
+    text-align: center;
+    transition: all .2s ease;
+}
+`;
+
+export const MenuItemBtn = styled.li`
+list-style: none;
+@media screen and (max-width:1000px){
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+    height: 120px;
+}
+`;
+
+export const MenuLinkBtn = styled(Link)`
+text-decoration: none;
+display: flex;
+justify-content: center;
+align-items: center;
+padding: 8px 16px;
+height: 100%;
+width: 100%;
+border: none;
+outline: none;
+`;
+
+
+const JFFooter = (props) => {
+  //click is the initial state and setclick will be the update state
+  const [click, setClick] = useState(false);
+
+  //Create a function to handle the click state of the menu icon. 
+  //if the menu icon was the menu bar at the beginning when clicked it will have the close icon
+  const handleClick = () => setClick(!click);
+
   return (
-    <Container {...props}>
-      <footer>
-        <div className="App-footer">
-          <ul>
-            <li>
-              <a href="#about">{moment().format("llll")}</a>
-            </li>
-            <li>
-              <a href="https://twitter.com/TCODEMONGER">Twitter</a>
-            </li>
-            <li>
-              <a href="https://github.com/JOHNFLEURIMOND">Github</a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/john-fleurimond">Linkedin</a>
-            </li>
-          </ul>
-        </div>
-      </footer>
-    </Container>
+    <Footer {...props}>
+      <GlobalStyle />
+
+      <FooterLogo to="/">
+        <MenuIcon />
+        Fleurimond
+      </FooterLogo>
+
+      <Menu>
+        <MenuItem>
+          <MenuLink onClick={handleClick} to="/">{moment().format('llll')}</MenuLink>
+        </MenuItem>
+        <MenuItem>
+          <MenuLink onClick={handleClick} to="https://twitter.com/TCODEMONGER">Twitter</MenuLink>
+        </MenuItem>
+        <MenuItem>
+          <MenuLink onClick={handleClick} to="https://github.com/JOHNFLEURIMOND">Github</MenuLink>
+        </MenuItem>
+        <MenuItem>
+          <MenuLink onClick={handleClick} to="https://www.linkedin.com/in/john-fleurimond">LinkedIn</MenuLink>
+        </MenuItem>
+      </Menu>
+    </Footer>
   );
 };
 
-const JFFooterWithStyle = styled(JFProjects, {
-  shouldForwardProp,
-})(baseFooterStyles, space, flexbox, typography);
-
-export default JFFooterWithStyle;
+export default JFFooter
--- assets/src/Navbar/Nav.tsx
+++ assets/src/Navbar/Nav.tsx
@@ -1,93 +1,190 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-import { css } from "@emotion/core";
-import styled from "@emotion/styled";
-import shouldForwardProp from "@styled-system/should-forward-prop";
-import { space, flexbox, typography } from "styled-system";
-import { Container } from "semantic-ui-react";
-
 import { fleurimondColors } from "../theme";
+import { Container } from '../layout/global-style';
+import { BiRestaurant, BiMenu, BiX } from 'react-icons/bi';
 
-const baseNavStyles = css({
-  boxSizing: "border-box",
-  fontSize: "20px",
-  height: "55px",
-  color: fleurimondColors.leapingLemon,
-  textAlign: "center",
-  position: "fixed",
-  width: "100vw",
-  backgroundColor: fleurimondColors.buttons.darkBlue,
-  minHeight: "5vh",
-  display: "flex",
-  padding: "20px",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  left: "0",
-  top: "0",
-  overflowX: "hidden",
-  zIndex: 100,
+import styled from 'styled-components';
 
-  ".App-header-nav ": {
-    fontSize: "20px",
-    height: "55px",
-    color: fleurimondColors.leapingLemon,
-    textAlign: "center",
-    padding: "20px",
-    position: "absolute",
-    left: "0",
-    top: "0",
-  },
-  li: {
-    listStyle: "none",
-  },
-  ul: {
-    width: "52vw",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    margin: 0,
-    padding: "1rem",
-  },
 
-  a: {
-    color: fleurimondColors.leapingLemon,
-    textDecoration: "none",
-  },
+export const Nav = styled.nav`
+  font-size: 18px;
+  width: 100%;
+  top: 0;
+  z-index: 999;
+  height: 80px;
+  background: linear-gradient(
+    90deg,
+    ${fleurimondColors.black} 0%,
+    ${fleurimondColors.smoke} 27%,
+    ${fleurimondColors.graySmoke} 85%
+  );
+  /* box-shadow: ${fleurimondColors.graySmoke} ; */
+  box-shadow: 0px 2px 10px ${fleurimondColors.graySmoke};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-  ".BannerHeader": {
-    width: "50%",
-  },
-  ".BannerHeader2": {
-    width: "50%",
-  },
-  "*": {
-    fontFamily: "Montserrat, sans-serif",
-  },
-});
+export const NavbarContainer = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+  ${Container};
+`;
 
-const JFNav = (props): JSX.Element => {
+export const NavLogo = styled(Link)`
+  color: ${fleurimondColors.white};
+  cursor: pointer;
+  margin: 1em;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  font-size: 2.5rem;
+  font-weight: 800;
+  transition: all 0.5s ease;
+  &:hover {
+    color: ${fleurimondColors.woooRed};
+    transform: scale(1.08);
+  }
+  @media (max-width: 1000px) {
+    display: none;
+  }
+`;
+
+export const NavIcon = styled(BiRestaurant)`
+  margin: 2rem;
+  transition: all 0.5s ease;
+  &:hover {
+    transform: scale(2);
+  }
+`;
+
+export const MenuIcon = styled.div`
+  display: none;
+  @media (max-width: 1000px) {
+    display: block;
+    position: absolute;
+    color: ${fleurimondColors.woooRed};
+    top: 0;
+    right: 0;
+    transform: translate(-50%, 20%);
+    font-size: 4rem;
+    cursor: pointer;
+  }
+`;
+
+export const Menu = styled.ul`
+  display: flex;
+  align-items: center;
+  text-align: center;
+  @media only screen and (max-width: 1000px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100vh;
+    position: absolute;
+    top: 80px;
+    left: ;
+    background: linear-gradient(
+      90deg,
+      ${fleurimondColors.black} 0%,
+      ${fleurimondColors.smoke} 27%,
+      ${fleurimondColors.white} 85%
+    );
+    transition: all 0.5s ease;
+  }
+`;
+
+export const MenuItem = styled.li`
+  list-style: none;
+  height: 80px;
+  @media only screen and (max-width: 1000px) {
+    width: 100%;
+    &:hover {
+      border: none;
+    }
+  }
+`;
+
+export const MenuLink = styled(Link)`
+  text-decoration: none;
+  font-weight: bold;
+  font-size: 2rem;
+  color: ${fleurimondColors.white};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+  height: 100%;
+  transition: all 0.2s ease;
+  &:hover {
+    color: ${fleurimondColors.woooRed};
+    transform: traslateY(-3rem);
+  }
+  &:active {
+    transform: traslateY(3rem);
+    color: ${fleurimondColors.woooRed};
+  }
+  @media only screen and (max-width: 1000px) {
+    display: block;
+    padding: 3rem;
+    text-align: center;
+    transition: all 0.2s ease;
+  }
+`;
+
+export const MenuItemBtn = styled.li`
+  list-style: none;
+  @media screen and (max-width: 1000px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+    height: 120px;
+  }
+`;
+
+export const MenuLinkBtn = styled(Link)`
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 16px;
+  height: 100%;
+  width: 100%;
+  border: none;
+  outline: none;
+`;
+
+
+const JFNav = (): JSX.Element => {//click is the initial state and setclick will be the update state
+  const [click, setClick] = useState(false);
+
+  //Create a function to handle the click state of the menu icon.
+  //if the menu icon was the menu bar at the beginning when clicked it will have the close icon
+  const handleClick = () => setClick(!click);
+
   return (
-    <Container className="App-header-nav" {...props}>
-      <header>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/CharacterPage">Pokemon Movesets</Link>
-          </li>
-          <li>
-            <Link to="/SignUp">About Us</Link>
-          </li>
-        </ul>
-      </header>
-    </Container>
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to='/'>Fleurimond</NavLogo>
+        <MenuIcon onClick={handleClick}>{click ? <BiX /> : <BiMenu />}</MenuIcon>
+
+        <Menu onClick={handleClick}>
+          <MenuItem>
+            <MenuLink onClick={handleClick} to='/CharacterPage'>
+              Pokemon Movesets
+            </MenuLink>
+            <MenuLink onClick={handleClick} to="/SignUp">
+              About Us
+            </MenuLink>
+          </MenuItem>
+        </Menu>
+      </NavbarContainer>
+    </Nav>
   );
 };
 
-const JFNavWithStyle = styled(JFNav, {
-  shouldForwardProp,
-})(baseNavStyles, space, flexbox, typography);
-
-export default JFNavWithStyle;
+export default JFNav;
