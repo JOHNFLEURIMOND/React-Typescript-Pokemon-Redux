fee45de72bc0ec3ab3609781afe0520a58856022
johnfleurimond <johncfleurimond@gmail.com>
2022-01-29 17:46:09 -0500

    Need to style about me page

--- assets/src/SearchBar/SearchBar.tsx
+++ assets/src/SearchBar/SearchBar.tsx
@@ -1,58 +1,148 @@
-import React, { useState, useEffect } from "react";
-import { css } from "@emotion/core";
-import styled from "@emotion/styled";
+import React, { useState } from "react";
 import { Animated } from "react-animated-css";
-import shouldForwardProp from "@styled-system/should-forward-prop";
-import { space, flexbox, typography } from "styled-system";
-import { Container } from "semantic-ui-react";
-import { Header } from "../Header";
 import { useDispatch, useSelector } from "react-redux";
 import { RootStore } from "../../Store";
 import { GetMarvelCharacter } from "../actions/MarvelActions";
+import styled from 'styled-components';
+import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react';
 
 import { fleurimondColors } from "../theme";
 
-const baseBannerStyles = css({
-  boxSizing: "border-box",
-  minWidth: 0,
-  fontSize: "1rem",
-  color: fleurimondColors.black,
-  lineHeight: "normal",
-  fontWeight: 600,
-  margin: 0,
-  padding: "0",
-  height: "700px",
-  width: "100%",
-  backgroundColor: fleurimondColors.white,
-  display: "grid",
-  gridTemplateColumns: "1fr 1fr 1fr",
-  gridAutoRows: "1fr 1fr 1fr",
 
-  img: {
-    width: "auto",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "right",
-    paddingLeft: "250px",
-  },
+export const ProjectsSectionContainer = styled.div`
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  grid-gap: 1rem;
+  width: 100%;
+  height: 700px;
+  box-sizing: border-box;
+  font-size: 1rem;
+  text-align: center;
+  font-weight: 500;
+  margin: 0;
+  padding: 2em;
+  line-height: normal;
+  background-color: ${fleurimondColors.white};
 
-  ".leftHalf": {
-    width: "50%",
-    display: "inline-block",
-  },
-  ".rightHalf": {
-    width: "50%",
-    display: "inline-block",
-  },
-  ".block": {
-    width: "100%",
-    display: "block",
-  },
-  "*": {
-    fontFamily: "Montserrat, sans-serif",
-  },
-});
+  @media (max-width: 800px) {
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 2em;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-gap: 10px;
+    justify-items: center;
+    align-items: center;
+  }
+`;
+
+export const Header = styled.h1`
+  font-size: 3rem;
+  text-align: center;
+  grid-column: span 3;
+
+  @media (max-width: 800px) {
+    font-size: 2rem;
+    text-align: center;
+    grid-column: span 2;
+  }
+`;
+
+export const CardDiv = styled.div`
+  grid-column: span 3;
+`;
+export const CineDiv = styled.div`
+  display: block;
+`;
+
+export const Link = styled.a`
+  font-size: 1rem;
+  font-weight: 700;
+  text-decoration: none;
+  color: ${fleurimondColors.black};
+`;
+
+export const ItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 30px;
+
+  div {
+    font-size: 18px;
+    margin-right: 5px;
+  }
+
+  span {
+    font-size: 15px;
+    font-weight: 500;
+  }
+
+  @media (max-width: 400px) {
+    display: none;
+  }
+`;
+
+export const Right = styled.div``;
+
+export const Hamburger = styled.span`
+  width: 32px;
+  height: 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  cursor: pointer;
+`;
+export const Bolt = styled.span`
+  position: relative;
+  margin: 50px;
+  width: 30px;
+  height: 50px;
+  transform-origin: 50% 50%;
+  transform: skewX(-30deg) skewY(-30deg) rotate(10deg);
+  background-color: yellow;
+  padding: 0;
+  margin-left: 0;
+  margin-right: 0;
+
+  &:before {
+    position: absolute;
+    border-style: solid;
+    border-width: 0 0 10px 5px;
+    border-color: transparent transparent ${fleurimondColors.graySmoke} transparent;
+    top: 0px;
+    left: -11px;
+    padding: 0;
+    margin: 0;
+    content: '';
+  }
+
+  &:after {
+    display: inline-block;
+    position: absolute;
+    border-style: solid;
+    border-width: 0 0 10px 5px;
+    border-color: transparent transparent transparent ${fleurimondColors.graySmoke};
+    bottom: 0px;
+    right: 3px;
+    content: '';
+  }
+`;
+export const FlippedCardInfoFieldset = styled.span`
+  color: ${fleurimondColors.smoke};
+  display: block;
+  font-size: 15px;
+  width: 100%;
+  font-family: 'proxima-nova', 'sans-serif';
+  font-weight: 500;
+  position: relative;
+  padding: 0 10px;
+  margin: 5px;
+`;
+
 
 
 const JFBanner = (props): JSX.Element => {
@@ -64,63 +154,55 @@ const JFBanner = (props): JSX.Element => {
   const handleSubmit = () => dispatch(GetMarvelCharacter(marvelCharacterName));
   console.log(setMessage);
 
-  useEffect(() => {
-    fetchData();
-  }, [])
-
-  const fetchData = async () => {
-    const result = await fetch(
-      "http://localhost:8080/backend/api"
+  if (!marvelState) {
+    return (
+      <div>
+        <Segment>
+          <Dimmer active>
+            <Loader size='massive'>....API Data Is Loading....</Loader>
+          </Dimmer>
+          <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+          <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+          <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+          <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+        </Segment>
+      </div>
     );
-    const data = await result.json();
-    console.log("data", data);
+  }
 
-    setMessage(data);
-  };
-
-  console.log(setMessage)
   return (
-    <Container {...props}>
+    <ProjectsSectionContainer {...props}>
       <Animated
         animationInDelay={0}
         animationIn="slideInUp"
         animationOut="slideOutDown"
         isVisible
       >
-
-
-          <Header className="BannerHeader2" as="h2" pt={50} pl={100}>
-            Pokemon Characters
-          </Header>
-          <Container {...props}>
-            <div className="block">
-              <input
-                onChange={handleChange}
-              />
-              <button type="submit" onClick={handleSubmit}>
-                {console.log("????")}
-                submit
-              </button>
-              <p> {message && JSON.stringify(message, null, 4)}</p>
+        <Header>
+          Pokemon Characters
+        </Header>
+        <CineDiv>
+          <input
+            onChange={handleChange}
+          />
+          <button type="submit" onClick={handleSubmit}>
+            submit
+          </button>
+          <p> {message && JSON.stringify(message, null, 4)}</p>
+        </CineDiv>
+        <CineDiv>
+          {marvelState.pokemon && (
+            <div>
+              <img src={marvelState.pokemon.sprites.front_default} alt="" />
+              {marvelState.pokemon.abilities.map(ability => {
+                return <p>{ability.ability.name}</p>
+              })}
             </div>
-            <div className="block">
-              {marvelState.pokemon && (
-                <div>
-                  <img src={marvelState.pokemon.sprites.front_default} alt="" />
-                  {marvelState.pokemon.abilities.map(ability => {
-                    return <p>{ability.ability.name}</p>
-                  })}
-                </div>
-              )}
-            </div>
-          </Container>
+          )}
+        </CineDiv>
       </Animated>
-    </Container>
+    </ProjectsSectionContainer>
   );
 };
 
-const JFBannerWithStyle = styled(JFBanner, {
-  shouldForwardProp,
-})(baseBannerStyles, space, flexbox, typography);
-
-export default JFBannerWithStyle;
+export default JFBanner;
